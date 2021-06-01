89c5d4c283b58e46fc58a94a9ff79690c11e1da0
rodirai <rodi_rumdaly@hotmail.com>
2021-06-01 22:39:00 +0930

    commit

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"net/http"
 )
 
-// User type to hold user details
+// User type
 type User struct {
 	Firstname string
 	Lastname  string
@@ -19,108 +19,109 @@ type User struct {
 	Password  string
 }
 
-// A container to hold templates
-var tpl *template.Template
-
-// Pointer
-var db *sql.DB
-
-// Create a variable that holds all the users
-// This will become redundant once we use database to save users
+var tpl *template.Template      // *template.Template is like a container that holds all the templates
+var db *sql.DB                  // Database pointer
 var dbUsers = map[string]User{} // userId, user
 
-// Create a variable that holds all the sessions
-// Save sessions in the app rather than database
+/*
+A session variable is a special type of variable whose value is maintained across subsequent web pages.
+With session variables, user-specific data can be preserved from page to page delivering customized
+content as the user interacts with the web application. Session variables normally exist until one of
+the follow criteria is met:
+1. the user closes the browser window;
+2. the maximum time allotment set on the server for session lifetime is exceeded;
+*/
 var dbSessions = map[string]string{} // sessionId, userId
 
 // init function always runs once
 func init() {
-	// Parse templates on start. This prepares the templates to be used later.
+	// Must is a helper that wraps a call to a function returning (*Template, error) and panics if the error is non-nil
+	// func Must(t *Template, err error) *Template
 	tpl = template.Must(template.ParseGlob("templates/*"))
 
-	// Connect to database
+	// Database connection
 	var err error
 	connStr := "postgres://postgres:password@localhost/postgres?sslmode=disable"
-	// Open opens a database specified by its database driver name and a driver-specific data source name, usually consisting of at least a database name and connection information.
+	// func Open(driverName string, dataSourceName string) (*DB, error)
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal("Invalid database config", err)
+		log.Fatal(err)
 	}
-
-	// Ping verifies a connection to the database is still alive, establishing a connection if necessary.
+	// Ping verifies a connection to the database is still alive, establishing a connection if necessary
+	// func (db *DB) Ping() error
 	if err = db.Ping(); err != nil {
-		log.Fatal("Database unreachable", err)
+		log.Fatal(err)
 	}
-	fmt.Println("You are connected to database")
+	fmt.Println("Successfully connected to database")
+	fmt.Println(db.Stats())
 }
 
 func main() {
-	// If the database connections is established in init do not close in init. This will close the connection immediately.
-	// Always close the connection in main
+	// Close closes the database and prevents new queries from starting
 	defer db.Close()
 
-	// Create handlers
+	// func HandleFunc(pattern string, handler func(ResponseWriter, *Request))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/welcome", welcome)
 
-	// Missing favicon handler. If the favicon is missing, this will handle the request.
+	// Handle registers the handler for the given pattern in the DefaultServeMux
+	// func Handle(pattern string, handler Handler)
 	http.Handle("favicon.ico", http.NotFoundHandler())
 
-	// Run local server.
-	// If you use Heroku, you dont need this. You have to user Heroku environment variable to run the server.
+	// func ListenAndServe(addr string, handler Handler) error
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
-	// Go to welcome page if the user is already logged in
-	if alreadyLoggedIn(request) {
-		// Go to welcome page if the user is already logged in
-		http.Redirect(writer, request, "/welcome", http.StatusSeeOther)
-		return
-	}
-	// Else, go to homepage
-	err := tpl.ExecuteTemplate(writer, "index.gohtml", dbUsers)
+	err := tpl.ExecuteTemplate(writer, "index.gohtml", dbSessions)
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 }
 
 func login(writer http.ResponseWriter, request *http.Request) {
-	// Go to welcome page if the user is already logged in
+	// Go to welcome page if user is already logged in
 	if alreadyLoggedIn(request) {
-		// Go to welcome page if the user is already logged in
 		http.Redirect(writer, request, "/welcome", http.StatusSeeOther)
 		return
 	}
-	// Else go to login page
-	// Process login if the method is GET
+	// Check if request method is POST
 	if request.Method == http.MethodPost {
-		// Retrieve values from form
 		enteredEmail := request.FormValue("email")
 		enteredPassword := request.FormValue("password")
 
-		// Check if userId exists
-		checkUser, ok := dbUsers[enteredEmail]
-		if !ok {
-			http.Error(writer, "User not found", http.StatusForbidden)
-			return
+		// Check if userId already exists
+		// QueryRow executes a query that is expected to return at most one row
+		// func (db *DB) QueryRow(query string, args ...interface{}) *Row
+		rows := db.QueryRow("SELECT * FROM user_login WHERE email = $1;", enteredEmail)
+
+		var user User
+		// Scan copies the columns from the matched row into the values pointed at by dest
+		// func (r *Row) Scan(dest ...interface{}) error
+		err := rows.Scan(&user.Firstname, &user.Lastname, &user.Email, &user.Password)
+		fmt.Println(user)
+		// ErrNoRows is returned by Scan when QueryRow doesn't return a row
+		// var ErrNoRows error = errors.New("sql: no rows in result set")
+		if err == sql.ErrNoRows {
+			panic(err)
 		}
 
 		// Does the entered password match the stored password?
-		err := bcrypt.CompareHashAndPassword([]byte(checkUser.Password), []byte(enteredPassword))
+		// CompareHashAndPassword compares a bcrypt hashed password with its possible plaintext equivalent.
+		// Returns nil on success, or an error on failure.
+		// func CompareHashAndPassword(hashedPassword []byte, password []byte) error
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(enteredPassword))
 		if err != nil {
-			http.Error(writer, "Password do not match", http.StatusForbidden)
-			return
+			panic("Wrong password")
 		}
 
-		// If both userId and password match, create new session.
-		sessionId, _ := uuid.NewV4()
+		// Create session id
+		sessionId := uuid.NewV4()
 
-		// Create new cookie
+		// Create cookie
 		cookie := &http.Cookie{
 			Name:     "session",
 			Value:    sessionId.String(),
@@ -137,23 +138,21 @@ func login(writer http.ResponseWriter, request *http.Request) {
 
 		// Redirect to homepage
 		http.Redirect(writer, request, "/welcome", http.StatusSeeOther)
+		return
 	}
-	// Else skip login process and display login form
+	// If request method is not POST, display signup page.
 	err := tpl.ExecuteTemplate(writer, "login.gohtml", nil)
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 }
 
 func logout(writer http.ResponseWriter, request *http.Request) {
-	// Go to homepage if the user is already logged in
+	// Go to homepage if the is already logged in
 	if !alreadyLoggedIn(request) {
-		// Go to login page if the user is not logged in
 		http.Redirect(writer, request, "/", http.StatusSeeOther)
 		return
 	}
-	// Else user is not logged in
 
 	// Get cookie
 	cookie, _ := request.Cookie("session")
@@ -173,32 +172,16 @@ func logout(writer http.ResponseWriter, request *http.Request) {
 
 	// Redirect to login page
 	http.Redirect(writer, request, "/login", http.StatusSeeOther)
+	return
 }
 
 func signup(writer http.ResponseWriter, request *http.Request) {
-	// Go to welcome page if the user is already logged in
+	// Go to welcome page if user is already logged in
 	if alreadyLoggedIn(request) {
-		// Go to welcome page if the user is already logged in
 		http.Redirect(writer, request, "/welcome", http.StatusSeeOther)
 		return
 	}
-	/*
-		Things to do -
-
-		1. Check if the method is post or not
-		2. Get form values
-		3. Check if userId already exists
-			3.1. Replaces dbUsers with database
-		4. Encrypt password
-		5. Create new user
-		6. Create new session
-		7. Create new cookie
-		8. Set cookie
-		9. Save session
-		10. Save new user
-	*/
-	// Else process sign up
-	// Process sign up if action is POST
+	// Check if request method is POST
 	if request.Method == http.MethodPost {
 		firstname := request.FormValue("firstname")
 		lastname := request.FormValue("lastname")
@@ -206,49 +189,41 @@ func signup(writer http.ResponseWriter, request *http.Request) {
 		password := request.FormValue("password")
 
 		// Check if userId already exists
-		rows, err := db.Query("SELECT email FROM user_login WHERE email = $1", email)
-		if err != nil {
-			panic(err)
-		}
-		// You have to close the rows after query
-		defer rows.Close()
+		rows := db.QueryRow("SELECT email FROM user_login WHERE email = $1;", email)
 
-		// Create a variable that will hold userId retrieved from query above
 		var userId string
-		// Loop through rows
-		// Next prepares the next result row for reading with the Scan method. It returns true on success, or false if
-		// there is no next result row or an error happened while preparing it.
-		for rows.Next() {
-			// Save rows into userId
-			// rows.Scan cannot run without rows.Next()
-			// Scan copies the columns in the current row into the values pointed at by dest.
-			// The number of values in dest must be the same as the number of columns in Rows
-			err = rows.Scan(&userId)
-			if err != nil {
-				panic(err)
-			}
+		// Scan copies the columns from the matched row into the values pointed at by dest
+		// func (r *Row) Scan(dest ...interface{}) error
+		err := rows.Scan(&userId)
+		// ErrNoRows is returned by Scan when QueryRow doesn't return a row
+		// var ErrNoRows error = errors.New("sql: no rows in result set")
+		if err != sql.ErrNoRows {
+			panic("User id not available")
 		}
 
-		// Encrypt password with bcrypt hash
+		// Encrypt password
+		// Do not save normal password
 		var encryptedPassword []byte
-		// GenerateFromPassword returns the bcrypt hash of the password at the given cost.
-		// If the cost given is less than MinCost, the cost will be set to DefaultCost, instead.
-		// Use CompareHashAndPassword, as defined in this package,
-		// to compare the returned hashed password with its cleartext version
 		// func GenerateFromPassword(password []byte, cost int) ([]byte, error)
 		encryptedPassword, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
+			panic(err)
 		}
 
 		// Create new user
 		newUser := User{firstname, lastname, email, string(encryptedPassword)}
 
-		// Create new session id
-		sessionId, _ := uuid.NewV4()
+		// func (s *Stmt) Exec(args ...interface{}) (Result, error)
+		_, err = db.Exec("INSERT INTO user_login (firstname, lastname, email, password) VALUES ($1, $2, $3, $4);",
+			newUser.Firstname, newUser.Lastname, newUser.Email, newUser.Password)
+		if err != nil {
+			panic(err)
+		}
 
-		// Create new cookie
+		// Creating UUID Version 4
+		sessionId := uuid.NewV4()
+
+		// Create cookie
 		cookie := &http.Cookie{
 			Name:     "session",
 			Value:    sessionId.String(),
@@ -258,106 +233,91 @@ func signup(writer http.ResponseWriter, request *http.Request) {
 		// Set cookie
 		http.SetCookie(writer, cookie)
 
-		// Save session
 		// dbSessions[key: d90af075-3969-44c9-8d4c-08d9858bdbd8] = value: test@test.com
 		// {key: d90af075-3969-44c9-8d4c-08d9858bdbd8, value: test@test.com}
 		dbSessions[cookie.Value] = email
 
-		// Save user
-		// dbUsers[key: test@test.com] = value: user{firstname, lastname, email, string(encryptedPassword)}
-		// {key: test@test.com, value: user{firstname, lastname, email, string(encryptedPassword)}}
-		dbUsers[email] = newUser
-
-		// Debug
-		fmt.Println("User:", newUser)
-		fmt.Println("Cookie:", cookie)
-		fmt.Println("DB Sessions:", dbSessions)
-		fmt.Println("DB Users:", dbUsers)
-
-		// Redirect to homepage
+		// After successful signup, go to welcome page.
 		http.Redirect(writer, request, "/welcome", http.StatusSeeOther)
 		return
 	}
-	// Else skip sign up process and display sign up form
+	// If request method is not POST, display signup page.
 	err := tpl.ExecuteTemplate(writer, "signup.gohtml", "Click to go back to homepage")
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 }
 
 func welcome(writer http.ResponseWriter, request *http.Request) {
-	// Go to welcome page if the user is already logged in
-	if !alreadyLoggedIn(request) {
-		// Go to welcome page if the user is already logged in
-		http.Redirect(writer, request, "/", http.StatusSeeOther)
-		return
-	}
-	err := tpl.ExecuteTemplate(writer, "welcome.gohtml", dbSessions)
+	// Query executes a query that returns rows, typically a SELECT. The args are for any placeholder parameters in the query.
+	// func (db *DB) Query(query string, args ...interface{}) (*Rows, error)
+	rows, err := db.Query("SELECT * FROM user_login;")
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
+	}
+	defer rows.Close()
+
+	var user User
+	// func (rs *Rows) Next() bool
+	users := make([]User, 0)
+	for rows.Next() {
+		// Scan copies the columns in the current row into the values pointed at by dest.
+		// The number of values in dest must be the same as the number of columns in Rows.
+		// func (rs *Rows) Scan(dest ...interface{}) error
+		err := rows.Scan(&user.Firstname, &user.Lastname, &user.Email, &user.Password)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	err = tpl.ExecuteTemplate(writer, "welcome.gohtml", users)
+	if err != nil {
+		panic(err)
 	}
 }
 
 func alreadyLoggedIn(request *http.Request) bool {
-	// Returns true if user is already logged in, else returns false.
-
-	/*
-		Things to do -
-
-		1. Get cookie from request
-		2. Return false if cookie doesn't exist
-		3. Return userId from dbSessions
-		4. Check if userId matches with any key in dbUsers
-		5. Return true
-	*/
-
-	// Get cookie
 	cookie, err := request.Cookie("session")
 	if err != nil {
-		// Return false if cookie is not found
 		return false
 	}
 
-	// Return userId from dbSessions where key is equals to cookie value
-	userId := dbSessions[cookie.Value]
-	// Check if userId matches with any key in dbUsers
-	_, ok := dbUsers[userId]
-	return ok
+	rows := db.QueryRow("SELECT * FROM user_login WHERE email = $1;", dbSessions[cookie.Value])
+
+	var user User
+	err = rows.Scan(&user)
+	if err != sql.ErrNoRows {
+		return true
+	}
+	return false
 }
 
 func getUser(writer http.ResponseWriter, request *http.Request) User {
-	/*
-		Things to do -
-
-		1. Get cookie from request
-		2. Create new session
-		3. Create new cookie
-		4. Set cookie
-		4. Check if userId matches with any key in dbSessions
-		5. Return user if exist
-	*/
 	cookie, err := request.Cookie("session")
 	if err != nil {
-		// Create new session id
-		sessionId, _ := uuid.NewV4()
-
-		// Create new cookie
+		sessionId := uuid.NewV4()
 		cookie := &http.Cookie{
 			Name:     "session",
 			Value:    sessionId.String(),
 			HttpOnly: true,
 		}
-
-		// Set cookie
 		http.SetCookie(writer, cookie)
 	}
 
-	// If the user already exists, get user.
+	// QueryRow executes a query that is expected to return at most one row
+	// func (db *DB) QueryRow(query string, args ...interface{}) *Row
+	rows := db.QueryRow("SELECT * FROM user_login WHERE email = $1;", cookie.Value)
+
 	var user User
-	if userId, ok := dbSessions[cookie.Value]; ok {
-		user = dbUsers[userId]
+	// Scan copies the columns from the matched row into the values pointed at by dest
+	// func (r *Row) Scan(dest ...interface{}) error
+	err = rows.Scan(&user.Firstname, &user.Lastname, &user.Email, &user.Password)
+	fmt.Println(user)
+	// ErrNoRows is returned by Scan when QueryRow doesn't return a row
+	// var ErrNoRows error = errors.New("sql: no rows in result set")
+	if err == sql.ErrNoRows {
+		panic(err)
 	}
 	return user
 }
